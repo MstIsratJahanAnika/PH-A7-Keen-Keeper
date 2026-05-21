27692f023d207bbda4d1a223dd66c445bfc49f1e
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-05-21 17:21:25 +0600

    button toggle in navbar added

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { GoGraph } from "react-icons/go";
 import { IoTimeOutline } from "react-icons/io5";
 import { RiHome2Line } from "react-icons/ri";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
     return (
@@ -16,18 +16,18 @@ const Navbar = () => {
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li className="text-white bg-[#244D3F] rounded-sm"><Link to="/"><RiHome2Line />Home</Link></li>
-                        <li className="text-[#64748B]"><Link to="/timeLine"><IoTimeOutline />TimeLine</Link></li>
-                        <li className="text-[#64748B]"><Link to="/stats"><GoGraph />Stats</Link></li>
+                        <li className=" rounded-sm"><NavLink to="/" className={({isActive})=> `flex items-center gap-2 rounded-sm px-4 py-2 ${isActive ? 'text-white bg-[#244D3F]': 'text-[#64748B]'}`}><RiHome2Line />Home</NavLink></li>
+                    <li className=" rounded-sm"><NavLink to="/timeLine" className={({isActive})=> `flex items-center gap-2 rounded-sm px-4 py-2 ${isActive ? 'text-white bg-[#244D3F]': 'text-[#64748B]'}`}><IoTimeOutline />TimeLine</NavLink></li>
+                    <li className=" rounded-sm"><NavLink to="/stats" className={({isActive})=> `flex items-center gap-2 rounded-sm px-4 py-2 ${isActive ? 'text-white bg-[#244D3F]': 'text-[#64748B]'}`}><GoGraph />Stats</NavLink></li>
                     </ul>
                 </div>
                 <a className="text-[#244D3F] font-semibold text-2xl"><span className="font-extrabold text-2xl text-[#1F2937]">Keen</span>Keeper</a>
             </div>
             <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li className="text-white bg-[#244D3F] rounded-sm"><Link to="/"><RiHome2Line />Home</Link></li>
-                    <li className="text-[#64748B]"><Link to="/timeLine"><IoTimeOutline />TimeLine</Link></li>
-                    <li className="text-[#64748B]"><Link to="/stats"><GoGraph />Stats</Link></li>
+                <ul className="menu menu-horizontal px-1 gap-2">
+                    <li className=" rounded-sm"><NavLink to="/" className={({isActive})=> `flex items-center gap-2 rounded-sm px-4 py-2 ${isActive ? 'text-white bg-[#244D3F]': 'text-[#64748B]'}`}><RiHome2Line />Home</NavLink></li>
+                    <li className=" rounded-sm"><NavLink to="/timeLine" className={({isActive})=> `flex items-center gap-2 rounded-sm px-4 py-2 ${isActive ? 'text-white bg-[#244D3F]': 'text-[#64748B]'}`}><IoTimeOutline />TimeLine</NavLink></li>
+                    <li className=" rounded-sm"><NavLink to="/stats" className={({isActive})=> `flex items-center gap-2 rounded-sm px-4 py-2 ${isActive ? 'text-white bg-[#244D3F]': 'text-[#64748B]'}`}><GoGraph />Stats</NavLink></li>
                 </ul>
             </div>
         </div>
