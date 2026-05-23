d298f451fb26785d004774b001282832269f2750
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-05-23 20:37:39 +0600

    loading spinner added

--- src/pages/homePage/allFriendsSection/AllFriends.jsx
+++ src/pages/homePage/allFriendsSection/AllFriends.jsx
@@ -1,23 +1,26 @@
+import { HashLoader } from "react-spinners";
 import EachFriendCard from "./eachFriendCard/EachFriendCard";
 
 const AllFriends = ({ allFriendsData, setAllFriendsData, isLoading, setIsLoading }) => {
     return (
         <div className="font-semibold text-2xl space-y-4 container mx-auto mt-20">
-            <h2>Your Friends</h2>
 
-            {/* dynamic friends data */}
-            <div className="grid grid-cols-4 gap-6">
-                {/* cards */}
-                {
-                    allFriendsData.map(friend => {
+            {
+                isLoading ? (<div className="flex justify-center items-center min-h-screen"><HashLoader color="#244D3F" /></div>) :
+                    (<>
+                        <h2>Your Friends</h2>
+                        <div className="grid grid-cols-4 gap-4 mt-6">
 
-                        return (
-                            <EachFriendCard key={friend.id} friend={friend}></EachFriendCard>
-                        );
-                    })
-                }
+                            {
+                                allFriendsData.map(friend => (
+                                    <EachFriendCard friend={friend} key={friend.id}></EachFriendCard>
+                                ))
+                            }
 
-            </div>
+                        </div>
+                    </>)
+            }
+
         </div>
     );
 };
