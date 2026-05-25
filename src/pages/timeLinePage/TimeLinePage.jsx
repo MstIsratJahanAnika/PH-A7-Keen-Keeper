67eddc72cb0c234f52301947a9d759ab8ff8eb35
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-05-26 02:03:19 +0600

    conditional rendering in TimeLine

--- src/pages/timeLinePage/TimeLinePage.jsx
+++ src/pages/timeLinePage/TimeLinePage.jsx
@@ -1,23 +1,40 @@
+import { CommunicationTypeContext } from "../../context/CommunicationTypeContext";
 import CallingInfos from "./callingInfos/CallingInfos";
 import TextingInfos from "./textingInfos/TextingInfos";
 import VideoCallInfos from "./videoCallInfos/VideoCallInfos";
+import { useContext } from "react";
+
+
 
 const TimeLinePage = () => {
 
+    const { callingInfos, textingInfos, videoCallInfos } = useContext(CommunicationTypeContext);
+
+    const noData =
+        callingInfos.length === 0 &&
+        textingInfos.length === 0 &&
+        videoCallInfos.length === 0;
     // context theke data access kora
     // const contextData = useContext(CommunicationTypeContext);
     // console.log(contextData, 'context data from timeLinePage');
 
     // directly destructuring korte chaile
 
-    
     return (
-        <div className="container mx-auto w-9/12 mt-20 mb-10">
+        <div className="container mx-auto min-h-[60vh] w-9/12 mt-20 mb-10">
             <h2 className="font-bold text-5xl mb-6">TimeLine</h2>
-
-            <CallingInfos></CallingInfos>
-            <TextingInfos></TextingInfos>
-            <VideoCallInfos></VideoCallInfos>
+            {
+                noData ?
+                    (
+                        <p className="text-center text-5xl text-[#244D3F] mt-40">No Data Found</p>
+                    ) : (
+                        <>
+                            <CallingInfos></CallingInfos>
+                            <TextingInfos></TextingInfos>
+                            <VideoCallInfos></VideoCallInfos>
+                        </>
+                    )
+            }
         </div>
     );
 };
